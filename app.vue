4f796d0aecca24d9d8eebdc3b195ce868613483c
future-html <witsanuporn19646@gmail.com>
2025-02-18 22:38:00 +0700

    todolist

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useCounter } from "./store/useCounter";
+console.log(useCounter);
+const store = useCounter();
+</script>
+
+<template>
+	<button @click="store.n++">Increment {{ store.n }}</button>
+</template>
---
+++ planning.ts
@@ -0,0 +1,12 @@
+// 1. todolist 
+// 2. authentication, board, invitation
+// 3. create and planning data first 
+// data  = user, boardInfo
+// - user info [{username, password, email}], prevent by middleware use global variable 
+// 3.1 isAuthenticated  = false, signin sucess ==> isAuthenticated = true 
+// - board info [{boardId, taskId, taskName, taskDescription, taskDueDate, assigneeName, assigneeId, status, priority}]
+// - assignee info (including in boardinfo)
+// 4. connect to database postgressql and testing by API 
+// 5. integrate with frontend 
+// authentication, board UI with figma 
+// 6. deploy to production p
