0ac45622b072116c192852a6e02d3476100c7713
future-html <witsanuporn19646@gmail.com>
2025-02-19 18:59:39 +0700

    add axios and get from backend

--- app.vue
+++ app.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { useCounter } from "./store/useCounter";
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
 console.log(useCounter);
 const store = useCounter();
+
+// setstate of data to be null
+const data = ref(null);
+
+onMounted(async () => {
+	try {
+		const response = await axios.get("http://localhost:3001/api/users");
+		data.value = response.data;
+		console.log(data.value);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+});
 </script>
 
 <template>
 	<button @click="store.n++">Increment {{ store.n }}</button>
+	{{ JSON.stringify(data, null, 2) }}
 </template>
--- planning.ts
+++ planning.ts
@@ -1,12 +1,13 @@
-// 1. todolist 
+// 1. todolist
 // 2. authentication, board, invitation
-// 3. create and planning data first 
+// 3. create and planning data first
 // data  = user, boardInfo
-// - user info [{username, password, email}], prevent by middleware use global variable 
-// 3.1 isAuthenticated  = false, signin sucess ==> isAuthenticated = true 
+// - user info [{username, password, email}], prevent by middleware use global variable
+// post to database and get the data when sign in
+// 3.1 isAuthenticated  = false, signin sucess ==> isAuthenticated = true
 // - board info [{boardId, taskId, taskName, taskDescription, taskDueDate, assigneeName, assigneeId, status, priority}]
 // - assignee info (including in boardinfo)
-// 4. connect to database postgressql and testing by API 
-// 5. integrate with frontend 
-// authentication, board UI with figma 
+// 4. connect to database postgressql and testing by API
+// 5. integrate with frontend
+// authentication, board UI with figma
 // 6. deploy to production p
